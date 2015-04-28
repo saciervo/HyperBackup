6549158866a73f4fa9d99af8abdf79823f461dae
Sandro Ciervo <sandro.ciervo@live.com>
2015-04-28 15:39:11 +0200

    Create Project for a Windows Service using Topshelf

---
+++ HyperBackup.Service/HyperBackupService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HyperBackup.Service
+{
+    internal class HyperBackupService
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HyperBackup.Service/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace HyperBackup.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(c =>
+            {
+                c.Service<HyperBackupService>(s =>
+                {
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                }); ;
+
+                c.SetServiceName("HyperBackup");
+                c.SetDisplayName("HyperBackup Service");
+                c.SetDescription("HyperBackup as a Service");
+            });
+        }
+    }
+}
